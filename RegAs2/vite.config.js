752c95e013104c96a83b7018997a28448d02a83d
xtfemcosegos <segos911@gmail.com>
2025-12-01 10:20:04 -0600

    Delete RegAs2 directory

--- RegAs2/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Configuración crucial para GitHub Pages: usa el nombre del repositorio ('/CIC/')
-export default defineConfig({
-  plugins: [react()],
-  base: '/CIC/', 
-})
