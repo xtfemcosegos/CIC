5511948311446120544209ced6782800228ce8a1
fraustop <fraustop@outlook.com>
2026-02-03 07:37:03 -0600

    Remplaza por CIC 7.1

---
+++ script.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDYS9u3_r8GEiEGj--1nIYkP09ic8be9PM",
+  authDomain: "cic7-1.firebaseapp.com",
+  projectId: "cic7-1",
+  storageBucket: "cic7-1.firebasestorage.app",
+  messagingSenderId: "608808369035",
+  appId: "1:608808369035:web:7660d3668becd2b4760d8e",
+  measurementId: "G-HWS509PML5"
+};
