ba66ef48892eea171b5d94857355b4ca7c06a2e6
xtfemcosegos <segos911@gmail.com>
2026-06-16 11:45:07 -0600

    primera entrega de marbetes

--- v1.0.0.1/sw.js
+++ v1.0.0.1/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cic-os-cache-1.0.0.800';
+const CACHE_NAME = 'cic-os-cache-1.0.0.825';
 
 self.addEventListener('install', event => {
     // Forzamos al Service Worker a instalarse de inmediato
